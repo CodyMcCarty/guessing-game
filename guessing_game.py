d9ca7bb7d35e932bce831d374343cc1d78372287
Cody McCarty <codyjmccarty@gmail.com>
2021-04-21 20:32:40 -0500

    alpha release

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,24 @@
 class GuessingGame():
 
+    def __init__(self):
+        import random
+        self.answer_number = random.randint(1,10)
+        self.is_solved = False
+        print(self.answer_number)
+        self.guess()
 
+    def guess(self):
+        player_guess = int(input("Guess a number between 1 and 10 "))
+        if player_guess < self.answer_number:
+            print( f"{player_guess} is too low")
+            self.guess()
+        elif player_guess > self.answer_number:
+            print( f"{player_guess} is too high")
+            self.guess()
+        else:
+            self.is_solved = True
+            print( f"{self.answer_number} is correct!")
+        
+
+new_game = GuessingGame()
+new_game #.guess()
